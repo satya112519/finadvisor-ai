adabe7b1ee6c35ad4611cd87dc2ec4618d9aad87
Anudeep Kadavergu <anudeepkadavergu@gmail.com>
2024-07-25 20:35:14 +0530

    test cicd

--- finadvisor-ai-cicd/app-restart.sh
+++ finadvisor-ai-cicd/app-restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose down --remove-orphans
-docker-compose pull
-docker-compose up -d
+docker compose down --remove-orphans
+docker compose pull
+docker compose up -d
 docker image prune -f
