a60fd72d867b59998ee537dcd9f9dd00c447b4a9
Anudeep Kadavergu <anudeepkadavergu@gmail.com>
2024-07-25 16:24:25 +0530

    added cors

--- finadvisor-ai-server/chatbot.py
+++ finadvisor-ai-server/chatbot.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # Allow CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Adjust according to your frontend URL
+    allow_origins=["*", "http://localhost:3000", "http://localhost:80", "http://35.188.50.151:3000", "http://35.188.50.151:80", "http://35.188.50.151:80" , "http://35.188.50.151:81"],  # Adjust according to your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
