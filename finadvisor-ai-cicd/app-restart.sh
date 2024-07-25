40d8bcd22bc9dae628f99972f048c2a7e54ad9c3
Anudeep Kadavergu <anudeepkadavergu@gmail.com>
2024-07-25 22:58:03 +0530

    sequential execution

--- finadvisor-ai-cicd/app-restart.sh
+++ finadvisor-ai-cicd/app-restart.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-docker compose down --remove-orphans
-docker rmi -f $(docker images -aq)
-docker volume prune  -f --all
-docker compose pull
-docker compose up -d
+# Stop and remove all containers, networks, and volumes associated with the Docker Compose file
+docker compose down --remove-orphans && \
+
+# Remove all Docker images (forcefully)
+docker rmi -f $(docker images -aq) && \
+
+# Remove all unused Docker volumes
+docker volume prune -f --all && \
+
+# Pull the latest images defined in the Docker Compose file
+docker compose pull && \
+
+# Start up the containers defined in the Docker Compose file in detached mode
+docker compose up -d && \
+
+# Remove dangling Docker images
 docker image prune -f
